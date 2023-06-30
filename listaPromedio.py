39d95d2f2fc9ca15cc63b5ffe9e56f0aae40b4ea
Erick1945 <114932536+Erick1945@users.noreply.github.com>
2023-06-30 18:12:04 +0000

    5notasXalumno

--- listaPromedio.py
+++ listaPromedio.py
@@ -1,3 +1,11 @@
 #@author: Erick De La Rosa
+promedio=[]
 print("Calcula el promedio de las 5 calificaciones de 'N' numero de alumnos")
 alumnos=input("Ingresa el numero de alumnos que avaluaras: ")
+for i in range(0,int(alumnos),1):
+  suma=0
+  for j in range(0,5,1):
+    nota=input("Ingresa la nota {j+1} del alumno {i+1}: ")
+    suma+=nota
+  promedio.append(suma/5)
+  print()
